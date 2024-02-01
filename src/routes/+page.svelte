094366da011d74fc8f4bd66f2b55607881725023
rhit-mohlerad <112584374+rhit-mohlerad@users.noreply.github.com>
2024-02-01 17:23:17 -0500

    Made basic front page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,8 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+
+<div style="text-align: center" class="container">
+    <h2>Like Google Keep, but better!</h2>
+    <p>Simplistic, effective webapp for Markdown editing</p>
+    <a role="button" href="">Get Started!</a>
+</div>
+
