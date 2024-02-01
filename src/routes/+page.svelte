1081ff85ab9da5feccd7aec14bbc0dce26f6c9df
rhit-mohlerad <112584374+rhit-mohlerad@users.noreply.github.com>
2024-02-01 17:36:03 -0500

    Added blank pages for login and signup

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -23,6 +23,6 @@
 <div style="text-align: center" class="container">
     <h2>Like Google Keep, but better!</h2>
     <p>Simplistic, effective webapp for Markdown editing</p>
-    <a role="button" href="">Get Started!</a>
+    <a role="button" href="/signup">Get Started!</a>
 </div>
 
---
+++ src/routes/signup/+page.svelte
@@ -0,0 +1,1 @@
+<h1>TODO: add signup here!</h1>
