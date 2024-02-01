6744f1ce0c17caf255c17b006d997aa2cd6c6bad
rhit-mohlerad <112584374+rhit-mohlerad@users.noreply.github.com>
2024-02-01 17:33:13 -0500

    Realized I'm a silly billy and put the nav in the wrong place

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,24 @@
+<style>
+    nav a h1, nav div a {
+        margin: 0.2em;
+        margin-bottom: 0;
+    }
+    nav {
+        margin-right: 0.5em;
+    }
 
+    .container {
+        margin-top: 10em;
+    }
+</style>
+
+<nav>
+    <a href="/"><h1>Notes</h1></a>
+    <div>
+        <a href="/login">Log In</a>
+        <a href="/signup" role="button">Sign Up</a>
+    </div>
+</nav>
 
 <div style="text-align: center" class="container">
     <h2>Like Google Keep, but better!</h2>
