0dabde50dc2f82fbee978770644a25bce1d5d196
Henry Morin <tronlover101@gmail.com>
2024-02-02 18:43:50 -0500

    typescript (yay!) things

--- svelte.config.js
+++ svelte.config.js
@@ -1,7 +1,9 @@
+import preprocess from 'svelte-preprocess';
 import adapter from '@sveltejs/adapter-auto';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
+	preprocess: preprocess(),
 	kit: {
 		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
 		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
