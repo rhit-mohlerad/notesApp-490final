715828c49648b776386451598bc812dce5d01970
Henry Morin <tronlover101@gmail.com>
2024-02-02 21:07:27 -0500

    required ssl for nondev deploy

--- src/lib/db/db.server.ts
+++ src/lib/db/db.server.ts
@@ -4,6 +4,6 @@ import postgres from 'postgres';
 import { DATABASE_URL } from '$env/static/private';
 import { dev } from '$app/environment';
 
-// const client = dev ? postgres(DATABASE_URL) : postgres(DATABASE_URL, { ssl: 'require' });
-const client =  postgres(DATABASE_URL) 
+const client = dev ? postgres(DATABASE_URL) : postgres(DATABASE_URL, { ssl: 'require' });
+//const client =  postgres(DATABASE_URL) 
 export const db = drizzle(client, {});
