dcc7940df006e0df1c5b4c3171a2ca3766f89187
rhit-mohlerad <112584374+rhit-mohlerad@users.noreply.github.com>
2024-02-05 17:22:34 -0500

    Redirects to homepage when logged in

--- src/routes/(navved)/+page.server.ts
+++ src/routes/(navved)/+page.server.ts
@@ -1,6 +1,12 @@
 import { redirect } from '@sveltejs/kit';
 import type { Actions, PageServerLoad } from './$types';
 
+export const load: PageServerLoad = ({locals}) => {
+    if (locals.email) {
+        throw redirect(303, '/homepage')
+    }
+}
+
 export const actions = {
     logout: async ({ cookies }) => {
         cookies.delete('sessionid', {path: '/'});
